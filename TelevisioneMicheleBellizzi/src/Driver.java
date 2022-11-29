2ebf8ddb0d6b84d86f080c95451460bae6f15a91
michelebellizzi <bellizzi23@gmail.com>
2022-11-29 14:50:09 +0100

    TVSeriale

--- TelevisioneMicheleBellizzi/src/Driver.java
+++ TelevisioneMicheleBellizzi/src/Driver.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Driver {
@@ -5,23 +6,40 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		stampa();
+		
+		
 
 	}
 
 	public static void stampa() {
-		
 		Scanner input= new Scanner(System.in);
+		
+		Elettrodomestici elettrodomestici = new Elettrodomestici();
+		
+		String Produttore=elettrodomestici.produttore;
+		int Seriale= elettrodomestici.NumSeriale;
+		System.out.println("inserire produttore:");
+		Produttore= input.next();
+		System.out.println("inserire il seriale");
+		Seriale=input.nextInt();
+		
 		String canale;
-		String[] canali = new String[10];
-		System.out.println("inserisci 10 canali:");
+		int numeroCanali;
+		System.out.println("quanti canali vuoi inserire: ");
+		numeroCanali=input.nextInt();
+		String[] canali = new String[numeroCanali];
+		System.out.println("inserisci  canali:");
 		for(int i=0; i<canali.length;i++) {
 			canale=input.next();
 			canali[i]=canale;
 		}
 	
 		
-TvMicheleBellizzi tvMicheleBellizzi= new TvMicheleBellizzi(canali);
-System.out.println(tvMicheleBellizzi);		
+TvMicheleBellizzi tvMicheleBellizzi= new TvMicheleBellizzi(Seriale,Produttore , canali);
+
+System.out.println(tvMicheleBellizzi);	
+tvMicheleBellizzi.toStringRicorsivo(numeroCanali-1, canali);
+Arrays.sort(canali);
 		
 		
 	}
--- TelevisioneMicheleBellizzi/src/TvMicheleBellizzi.java
+++ TelevisioneMicheleBellizzi/src/TvMicheleBellizzi.java
@@ -1,18 +1,16 @@
 import java.util.Arrays;
 
-public class TvMicheleBellizzi {
+public class TvMicheleBellizzi extends Elettrodomestici{
 
 	String[] canali;
 
-	public TvMicheleBellizzi(String[] canali) {
-		
+	
+
+	public TvMicheleBellizzi(int numSeriale, String produttore, String[] canali) {
+		super(numSeriale, produttore);
 		this.canali = canali;
 	}
 
-	@Override
-	public String toString() {
-		return "TvMicheleBellizzi [canali=" + Arrays.toString(canali) + "]";
-	}
 
 	public String[] getCanali() {
 		return canali;
@@ -21,4 +19,31 @@ public class TvMicheleBellizzi {
 	public void setCanali(String[] canali) {
 		this.canali = canali;
 	}
-}
+
+	
+	public String toStringRicorsivo(int n, String s[]){
+	    if(n==0){
+	        System.out.println(s[n]);
+	        return "fine";
+	    }
+	    System.out.println(s[n]);
+
+	    return toStringRicorsivo(n-1 ,s);
+	    
+	    
+	}
+
+
+	@Override
+	public String toString() {
+		return "TvMicheleBellizzi [canali=" + Arrays.toString(canali) + ", NumSeriale=" + NumSeriale + ", produttore="
+				+ produttore + "]";
+	}
+	
+	
+	
+	
+	}
+
+
+
