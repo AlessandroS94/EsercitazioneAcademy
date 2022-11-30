bd265b9e4d6a3fb81f6b4d9e16ce972f43997d68
Domenico-morelli <domenico.morelli@studenti.unipg.it>
2022-11-30 10:05:41 +0100

    Prima commit del Giorno

--- TvDomenicoMorelli/src/tvprj/driver/Driver.java
+++ TvDomenicoMorelli/src/tvprj/driver/Driver.java
@@ -1,16 +1,60 @@
 package tvprj.driver;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import tvprj.impl.TV;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		
-		 String[] strAr1=new String[] {"italia1", "italia2", "italia3","Radio Maria","MTV"};
-		System.out.println("inizio");
+		short ctrl = 0;
+		List<String> list = new ArrayList<>();
+
+		list.add("italia1");
+		list.add("italia2");
+		list.add("italia3");
+		list.add("Radio Maria");
+		list.add("MTV");
 		TV tv = new TV();
-		tv.setCanali(strAr1);
-			
-		tv.cambiaCanale("italia1");
+		tv.setCanali(list);
+		Scanner sc = new Scanner(System.in);
+		String canaleUtente = null;
+		do {
+
+			System.out.println("C'a ma fa'?\n");
+			System.out.println("1) Aggiungi canale");
+			System.out.println("\n2) Cambia canale");
+			System.out.println("\n3) Rimuovi canale");
+			ctrl=sc.nextShort();
+			sc.nextLine();
+			switch (ctrl) {
+			case 1:
+				System.out.println("Inserisci il nome del canale da aggiungere");
+				canaleUtente = sc.nextLine();
+				tv.addCanale(canaleUtente);
+				break;
+			case 2:
+				System.out.println("Inserisci il nome del canale da visualizzare");
+				canaleUtente = sc.nextLine();
+				tv.cambiaCanale(canaleUtente);
+				break;
+			case 3:
+				tv.cambiaCanale(null);
+				break;
+			case 4:
+				tv.printCanali();
+				break;
+
+			default:
+				System.out.println("Valore non accettato: " + ctrl);
+			}
+		} while (ctrl != 1 && ctrl != 2 && ctrl != 3&& ctrl != 4);
+
+		System.out.println("Ecco tutti i canali");
+		tv.printCanali();
+		
 	}
 }
--- TvDomenicoMorelli/src/tvprj/impl/TV.java
+++ TvDomenicoMorelli/src/tvprj/impl/TV.java
@@ -2,39 +2,36 @@ package tvprj.impl;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import tvprj.interfaces.Hub;
 import tvprj.interfaces.Telecomando;
 import tvprj.interfaces.Telefono;
 
 public class TV extends Elettrodomestici implements Hub, Telefono, Telecomando {
-	String[] canali;
+	List<String> canali;
 	String canaleCorrente;
-
-	public TV(String produttore, int seriale, String[] canali, String canaleCorrente) {
-		super(produttore, seriale);
-		this.canali = canali;
-		this.canaleCorrente = canaleCorrente;
-	}
 
 	public TV() {
 		super();
 	}
 
-	public String[] getCanali() {
+	public List<String> getCanali() {
 		return canali;
 	}
 
-	public void setCanali(String[] canali) {
+	public void setCanali(List<String> canali) {
 		this.canali = canali;
+	}
+
+	@Override
+	public String toString() {
+		return "TV [canali=" + canali + ", canaleCorrente=" + canaleCorrente + ", produttore=" + produttore
+				+ ", seriale=" + seriale + "]";
 	}
 
 	public String getCanaleCorrente() {
 		return canaleCorrente;
-	}
-
-	public void setCanaleCorrente(String canaleCorrente) {
-		this.canaleCorrente = canaleCorrente;
 	}
 
 	public String recoursiveToString(int n, String[] arr) {
@@ -48,28 +45,33 @@ public class TV extends Elettrodomestici implements Hub, Telefono, Telecomando {
 	}
 
 	@Override
-	public String toString() {
-		return "TV [canali=" + Arrays.toString(canali) + ", produttore=" + produttore + ", seriale=" + seriale + "]";
+	public void cambiaCanale(String canaleUtente) {
+		this.canaleCorrente = canaleUtente; 
+		
 	}
-
-	@Override
-	public void cambiaCanale(String canale) {
-		if (this.canali == null) {
-			System.out.println("l'array e' vuoto");
-		} else {
-			int i = 0;
-			do {
-
-				if (canale == canali[i]) {
-					this.canaleCorrente = canale;
-					System.out.println("Il canale " + canale + " e' presente");
-				}
-				++i;
-			} while (canale == canali[i] || i == canali.length);
-			if (i == canali.length)
-				System.out.println("Canale non presente");
+	
+	public void addCanale(String canaleUtente) {
+		this.canali.add(canaleUtente); 
+		
+	}
+	
+	public void printCanali() {
+		for(String str:canali) {
+			System.out.println(str);
 		}
-
 	}
 
+	/*
+	 * @Override public void cambiaCanale(String canale) { if (this.canali == null)
+	 * { System.out.println("l'array e' vuoto"); } else { int i = 0; do {
+	 * 
+	 * if (canale.equals(canali[i])) { this.canaleCorrente = canale;
+	 * System.out.println("Il canale " + canale + " e' presente"); } ++i; } while
+	 * (canale == canali[i] || i == canali.length);
+	 * 
+	 * /* errore * if (i == canali.length)
+	 * System.out.println("Canale non presente"); }
+	 * 
+	 * }
+	 */
 }
