6a35706d14f3b90acade824b404b8ca7436be979
GrandeDito <grandedito9@gmail.com>
2022-11-30 10:39:22 +0100

    arrayList

--- TVAdriano/src/com/tv/TVAdriano.java
+++ TVAdriano/src/com/tv/TVAdriano.java
@@ -1,6 +1,7 @@
 package com.tv;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.tv.interfaces.iHub;
@@ -10,36 +11,73 @@ import com.tv.interfaces.iTelefono;
 public class TVAdriano extends Elettrodomestico implements iHub, iTelecomando, iTelefono {
 
 	public static void main(String[] args) {
-		String[] canali = { "canale 5", "rete 4", "mediaset extra", "italia juve", "telenorba", "radio maria",
-				"TV2000"};
-		//System.out.println("inserisci il canale nuovo: ");
-		//Scanner input= new Scanner(System.in);
-		String programmavisione = "canale 5";
-		String nuovoCanale = "rete 4";
-		Boolean canalePresente;
+		// String[] canali = { "canale 5", "rete 4", "mediaset extra", "italia juve",
+		// "telenorba", "radio maria",
+		// "TV2000" };
 		
-		
+		ArrayList<String> canali = new ArrayList<>();
 		TVAdriano tv = new TVAdriano();
+		String programmavisione = "canale 12";
+		Boolean canalePresente;
 		tv.setNumeroSeriale("12e43232se");
 		tv.setProduttore("franco");
-		System.out.println(programmavisione);
+		System.out.println("inserisci un nuovo canale nella lista: ");
+		Scanner input = new Scanner(System.in);
+		String nuovoCanaleLista;
+		try {
+			nuovoCanaleLista = input.nextLine();
+			System.out.println(nuovoCanaleLista);
+			tv.aggiungiCanale(canali, nuovoCanaleLista);
+			
+		} catch (InputMismatchException e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}		
+		canali.add("rete 4");
+		canali.add("canale 12");
+		canali.add("francesco demartino");
+		stampa(canali);
+
+		System.out.println("inserisci canale in cui cambiare: ");
+		String nuovoCanale="";
+		try {
+			nuovoCanale = input.nextLine();
+			System.out.println(nuovoCanale);
+		} catch (InputMismatchException e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 		
-		canalePresente= controlloCanale(canali, nuovoCanale);
+
+		System.out.println("stavi vedendo " + programmavisione);
+
+		canalePresente = controlloCanale(canali, nuovoCanale);
+
+		System.out.println("inserisci canale da eliminare: ");
+		String eliminare = "";
+		try {
+			eliminare =  input.nextLine();
+			tv.eliminaCanale(canali, eliminare);
+		} catch (InputMismatchException e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 		
-		if(canalePresente==true) {
+			if (canalePresente == true) {
 			tv.cambiaCanale(nuovoCanale);
 			System.out.println(nuovoCanale);
-			
+
 		} else {
 			System.out.println("il canale non è presente");
 		}
-		// stampa(canali);
+		stampa(canali);
+		
 		// System.out.println("dispositivo " + tv.getNumeroSeriale() + " a cura di " +
 		// tv.getNumeroSeriale());
-		//System.out.println(nuovoCanale);
+		 System.out.println("attualmente stai vedendo "+nuovoCanale);
 	};
 
-	private String[] canali;
+	private ArrayList<String> canali;
 	private String programmavisione;
 
 	public TVAdriano() {
@@ -51,32 +89,33 @@ public class TVAdriano extends Elettrodomestico implements iHub, iTelecomando, i
 		// TODO Auto-generated constructor stub
 	}
 
-	public String[] getCanali() {
+	public ArrayList<String> getCanali() {
 		return canali;
 	}
 
-	public void setCanali(String[] canali) {
+	public void setCanali(ArrayList<String> canali) {
 		this.canali = canali;
 	}
 
 	@Override
 	public String toString() {
-		return "TVadriano [canali=" + Arrays.toString(canali) + "]";
+		return "TVAdriano [canali=" + canali + ", programmavisione=" + programmavisione + "]";
 	}
 
-	public static void stampa(String[] canali) {
-		for (String canale : canali) {
-			System.out.println("sei su" + canale + "!");
+	public static void stampa(ArrayList<String> canali2) {
+		for (String canale : canali2) {
+			System.out.println("sei su " + canale + "!");
 		}
 	}
 
-	public static Boolean controlloCanale(String[] canali, String Nuovo) {
+	public static Boolean controlloCanale(ArrayList<String> canali, String Nuovo) {
 		for (String canale : canali) {
 			if (canale == Nuovo) {
 				System.out.println("il " + Nuovo + " selezionato corrisponde a " + canale + "!");
 				return true;
-			} 
-		}return false;
+			}
+		}
+		return false;
 	}
 
 	/*
@@ -98,6 +137,24 @@ public class TVAdriano extends Elettrodomestico implements iHub, iTelecomando, i
 	public void cambiaCanale(String n) {
 		// TODO Auto-generated method stub
 		this.programmavisione = n;
+	}
+
+	public void eliminaCanale(ArrayList<String> canali, String ce) {
+	/*	int index = 0;
+		for (String i : canali) {
+			if (i == ce) {
+				System.out.println(i);
+				canali.remove(index);
+			}
+			index++;
+		} */
+		canali.remove(ce);
+		this.canali = canali; 
+	}
+
+	public void aggiungiCanale(ArrayList<String> canali, String cn) {
+		canali.add(cn);
+		this.canali = canali;
 	}
 
 }
